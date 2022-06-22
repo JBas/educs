837cc35beddb019548b3be984bde5d00058b9a59
Joshua Bas <joshua.n.bas@gmail.com>
2022-06-22 15:12:24 +0000

    pushing to main

--- educs/__init__.py
+++ educs/__init__.py
@@ -1,11 +1,12 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-from educs.pygame_wrapper import (setup, draw, createCanvas, background, fill, noFill, line, ellipse, circle, rect, quad, arc, triangle, push, pop, getMouseX, getMouseY, getWidth, getHeight, cursor, strokeWeight, stroke, noStroke, loadImage, image, keyPressed, mouseClicked, mouseDragged, isMousedPressed, Image, frameRate, loop, noLoop)
+from educs.pygame_wrapper import (setup, draw, createCanvas, line, ellipse, circle, rect, quad, arc, triangle, push, pop, getMouseX, getMouseY, getWidth, getHeight, cursor, strokeWeight, stroke, noStroke, loadImage, image, keyPressed, mouseClicked, mouseDragged, isMousedPressed, Image, frameRate, loop, noLoop)
 
-from educs.color import (color, alpha,
-                        red, green, blue, brightness,
-                        hue, lightness, saturation, lerpColor)
+from educs.color import (color, alpha, background,
+                         fill, noFill,
+                         red, green, blue, brightness,
+                         hue, lightness, saturation, lerpColor)
 
 from educs.constants import (TWO_PI, HALF_PI, PI, QUARTER_PI, TAU, CURSOR_ARROW, CURSOR_DIAMOND, CURSOR_BROKEN_X, CURSOR_TRI_LEFT, CURSOR_TRI_RIGHT)
 
